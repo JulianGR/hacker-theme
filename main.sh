cb5941d4d01924fcbbcf0b06b981ebe01e00fcda
JulianGR <juliangr505@gmail.com>
2022-11-09 14:15:24 +0100

    code shorcut

--- main.sh
+++ main.sh
@@ -170,7 +170,7 @@ sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update
 sudo apt install code
-
+rm microsoft.pgp
 
 # ===================================
 # =========== CONSOLE ===============
